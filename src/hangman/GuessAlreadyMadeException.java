0fe6dce0a7565fb15996eb80c8e2019cfa098ace
David Lindell <lindell@mers.byu.edu>
2014-05-09 21:30:07 -0600

    Uploading evilhangman project

---
+++ src/hangman/GuessAlreadyMadeException.java
@@ -0,0 +1,12 @@
+package hangman;
+
+public class GuessAlreadyMadeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GuessAlreadyMadeException() {
+		
+	}
+}
