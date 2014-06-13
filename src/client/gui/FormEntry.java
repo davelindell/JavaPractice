a6c0841359e774144abe3b38cc5dad572b44aa0e
davelindell <davelindell@hotmail.com>
2014-06-12 20:31:46 -0600

    started form entry

--- src/client/gui/FormEntry.java
+++ src/client/gui/FormEntry.java
@@ -3,15 +3,28 @@ package client.gui;
 import java.awt.Dimension;
 
 import javax.swing.JComponent;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 
 public class FormEntry extends JPanel {
+	FormEntryModel form_model = new FormEntryModel();
+	
 	public FormEntry() {
 		createComponents();
 	}
 	
 	private void createComponents() {
 		this.setPreferredSize(new Dimension(600, 200));
+		
+		JList row_list = new JList();
+		this.form_model = new FormEntryModel();
+		row_list.setModel(this.form_model);
+		
+		
+		JSplitPane split_pane = new JSplitPane();
 
 	}
 }
---
+++ src/client/gui/FormEntryModel.java
@@ -0,0 +1,32 @@
+package client.gui;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
+
+public class FormEntryModel implements ListModel<Integer> {
+
+	@Override
+	public void addListDataListener(ListDataListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer getElementAt(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeListDataListener(ListDataListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
